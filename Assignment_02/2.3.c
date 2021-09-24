074b0afe3c739d55255443b578cd1ba8a560b57e
Shraddha Barge <63111483+Shraddha0550@users.noreply.github.com>
2021-09-23 22:18:58 -0400

    Assignment 02

---
+++ Assignment_02/2.3.c
@@ -0,0 +1,32 @@
+. Accept two numbers from user and display first number in second
+number of times.
+Input : 12 5
+Output : 12 12 12 12 12
+Input : -2 3
+Output : -2 -2 -2
+Input : 21 -3
+Output : 21 21 21
+Input : -2 0
+Output :
+#include<stdio.h>
+______ Display( _____ iNo, int iFrequency)
+{
+ int int = 0;
+
+ // Write Updater
+ for(______; _______; ______)
+ {
+ printf(“_____”,______);
+ }
+}
+int main()
+{
+ int iValue = 0;
+ int iCount = 0;
+ printf(“Enter number”);
+ scanf(“____” ,&____);
+ printf(“Enter frequency”);
+ scanf(“____” ,&____);
+ Display(_____, ______);
+ return 0;
+}
---
+++ Assignment_02/2.4.c
@@ -0,0 +1,29 @@
+/*
+    Accept two numbers from user and display first number in second number of times.
+
+*/
+
+#include<stdio.h>
+Display(int iNo, int iFrequency)
+{
+    int i = 0;
+     for(i = 0 ; i < iFrequency ; i++)
+     {
+        printf("%d\t",iNo);
+     }
+}
+int main()
+{
+     int iValue = 0;
+     int iCount = 0;
+
+     printf("Enter number");
+     scanf("%d" ,&iValue);
+
+     printf("Enter frequency");
+     scanf("%d" ,&iCount);
+
+     Display(iValue, iCount);
+
+     return 0;
+}

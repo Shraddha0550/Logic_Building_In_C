8d609962ab8b0aee5843f801ee242b40d53ea479
Shraddha Barge <63111483+Shraddha0550@users.noreply.github.com>
2021-12-01 12:06:03 -0500

    Assignment no 10

---
+++ Assignment_10/10.5.c
@@ -0,0 +1,29 @@
+/**
+Write a program which accept area in square feet and convert it into square
+meter. (1 square feet = 0.0929 Square meter)
+Input : 5
+Output : 0.464515
+
+*/
+
+#include<stdio.h>
+double  SquareMeter(int iNo)
+{
+    float Ret = 0;
+    Ret = iNo *  0.0929 ;
+
+    return Ret;
+}
+int main()
+{
+    int iValue = 0;
+    double dRet = 0.0;
+    printf("Enter area in square feet =");
+    scanf("%d",&iValue);
+
+    dRet = SquareMeter(iValue);
+
+    printf("After conversion of square feet to square  Meter = %f",dRet);
+
+    return 0;
+}
